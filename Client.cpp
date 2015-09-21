c1db4fa569773290535889f158d24cf39ffc40e5
JChieftainK <JK@Justins-MacBook-Pro.local>
2015-09-21 14:32:27 -0500

    Added Forking
    Added forking and executing

--- Client.cpp
+++ Client.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
-#include <unistd.h>
+#include <unistd.h>    //execl
+#include <sys/types.h> //mode_t
+#include <sys/stat.h>  //mode_t mkfifo
+#include <iomanip>     //std::localtime
 
 int main(int argc, char *argv[]) {
-	std::cout << "Hello Client\n";
-	execl("Server",NULL);
-	std::cout << "Goodbye Client\n";
+	std::time_t time = std::time(nullptr);
+	std::cout << "Client: PID " << getpid() <<  " - "
+					<< std::put_time(std::localtime(&time),"%F_%T") << '\n';
+	
+	if(mkfifo("ctos", S_IRUSR | S_IWUSR) < 0) {
+		std::cout << "Failed\n";	
+	}
+	
+	pid_t pid = fork();
+	if(pid == 0){
+		std::cout << pid << " Child Process " << getpid() << '\n';
+		execl("Server",NULL);
+		std::cout << "After Exec\n";
+	}
+	
+	
+	
+	std::cout << pid << " Goodbye Client " << getpid() << '\n';
 }
--- Server.cpp
+++ Server.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
-
-int main(int argc, char *argv[]) {
-	std::cout << "Hello Server\n";
+#define fifo "/home/justijk/ctos"
+int main() {
+	open(fifo, O_RDONLY);
+	
+	/*if(mkfifo(fifo, 0666) < 0) {
+		return 1;
+	}
+	
+	int fifo_int = open(fifo, O_RDONLY);
+	if(fifo_int < 0) {
+		return 2;
+	}
+	
+	char temp[32];
+	if(read(fifo_int, temp, sizeof(temp)) < 0) {
+		return 3;
+	}
+	*/
+	
 }
